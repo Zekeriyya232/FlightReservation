82033e191f76b5a42d39d4160ef9d47c3b399594
Zekeriyya Yunus Sönmez <zyunussonmez@gmail.com>
2023-12-17 16:18:38 +0300

    Koltuk seçim işlemleri yapıldı

---
+++ FlightReservation/Migrations/20231213193121_tabloGüncelleme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebProje2023.Migrations
+{
+    public partial class tabloGüncelleme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "biletFiyat",
+                table: "Ticket",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "tarihKalkis",
+                table: "Ticket",
+                type: "Date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "biletFiyat",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "tarihKalkis",
+                table: "Ticket");
+        }
+    }
+}
